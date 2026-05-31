597157d7ce95664f629bee18e02ed1d2c1d34a10
Nachit0C <nachociccone@gmail.com>
2026-05-30 22:42:02 -0300

    cambio diseño de input de busqueda

--- features/dashboard/admin/components/ingredientes/IngredientesHeader.tsx
+++ features/dashboard/admin/components/ingredientes/IngredientesHeader.tsx
@@ -14,7 +14,7 @@ export default function IngredientesHeader({ handleSearch }: { handleSearch: (bu
                 <Input
                     type="text"
                     placeholder="Buscar por nombre..."
-                    className="w-full pl-9 pr-4 py-2 rounded-full border border-gray-200 bg-gray-50 text-sm text-gray-700 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-transparent transition"
+                    className="w-full pl-9 pr-4 py-5 bg-background rounded-xl border border-gray-200 text-sm text-gray-700 placeholder:text-gray-400 focus:outline-none focus:ring-2 focus:ring-green-400 focus:border-transparent transition"
                     onKeyDown={(e) => {
                         if (e.key === "Enter") handleSearch(e.currentTarget.value)
                     }}
